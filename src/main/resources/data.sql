ae4d4b94d7eaf2b67279ffa8db34221233a059ae
mohammad <shalabimobile@gmail.com>
2016-11-20 10:39:49 +0100

    fixing database.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,17 @@
-INSERT INTO Audiologists(id,first_name,last_name)VALUES(1, 'Mohammad', 'Shalabi');
-INSERT INTO Audiologists(id,first_name,last_name)VALUES(2, 'Raluca', 'Moldovan');
-INSERT INTO Audiologists(id,first_name,last_name)VALUES(3, 'Otto', 'Wesendonk');
+INSERT INTO Audiologists(id,first_name,last_name) --VALUES(1, 'Mohammad', 'Shalabi');
+SELECT 1, 'Mohammad', 'Shalabi'
+FROM Audiologists
+WHERE NOT EXISTS (SELECT 1 FROM Audiologists WHERE id=1);
 
+INSERT INTO Audiologists(id,first_name,last_name) --VALUES(2, 'Raluca', 'Moldovan');
+SELECT 2, 'Raluca', 'Moldovan'
+FROM Audiologists
+WHERE NOT EXISTS (SELECT 1 FROM Audiologists WHERE id=2);
+
+
+INSERT INTO Audiologists(id,first_name,last_name)  --VALUES(3, 'Otto', 'Wesendonk');
+SELECT 3, 'Otto', 'Wesendonk'
+FROM Audiologists
+WHERE NOT EXISTS (SELECT 1 FROM Audiologists WHERE id=3);
 
 
